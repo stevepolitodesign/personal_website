bf61aac65a45b2cbe090ab2460c41f9513a51d20
Steve Polito <stevepolito@hey.com>
2022-12-09 15:28:13 -0500

    Test build
    This commit tests that specific files are included or excluded from the
    build.

--- test/personal_site_test.rb
+++ test/personal_site_test.rb
@@ -1,6 +1,18 @@
 require "test_helper"
 
 class BuildTest < IntegrationTestCase
+  def test_exluded_files
+    refute_exist("Rakefile")
+    refute_exist("test")
+    refute_exist("README.md")
+    refute_exist("bin")
+  end
+
+  def test_included_files
+    assert_exist("_redirects")
+    assert_exist("404.html")
+  end
+
   def test_redirects
     expected = <<~TEXT
       /categories/ja-mstack /categories/jamstack
