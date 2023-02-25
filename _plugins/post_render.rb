afad2e08c911bd2cd79cea8a5e7e0fc818300122
Steve Polito <stevepolito@hey.com>
2023-02-25 12:02:08 -0500

    Make post headlines anchor links
    Adds an anchor link to each headline on each post.

--- _plugins/post_render.rb
+++ _plugins/post_render.rb
@@ -1,5 +1,7 @@
 require "./lib/jekyll-image-link"
+require "./lib/jekyll-anchor-link"
 
 Jekyll::Hooks.register [:documents], :post_render do |doc|
   Jekyll::ImageLink.new(doc).link_images
+  Jekyll::AnchorLink.new(doc).link_headings
 end
