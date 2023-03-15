10193a09924ed6ab34f6cff0bbd5be705a051593
MICHAEL ARRIAGA <m.arriaga.smb@gmail.com>
2023-03-15 12:46:03 -0700

    add copy code button to post layout code blocks

---
+++ assets/js/code_clipboard.js
@@ -0,0 +1,31 @@
+const codeBlocks = document.querySelectorAll("pre");
+
+codeBlocks.forEach(function (codeBlock) {
+  const Button = document.createElement("button");
+  Button.className = " ";
+  Button.type = "button";
+  Button.ariaLabel = "Copy code to clipboard";
+  Button.innerText = "Copy";
+  Button.style.position = "absolute";
+  Button.style.top = 0;
+  Button.style.right = 0;
+  Button.style.backgroundColor = "black";
+  Button.style.border = "1px solid #535353";
+  Button.style.color = "white";
+  Button.style.padding = "3px 10px";
+  Button.style.fontSize = "14px";
+
+  codeBlock.append(Button);
+
+  Button.addEventListener("click", () => {
+    const code = codeBlock.querySelector("code").innerText.trim();
+    window.navigator.clipboard.writeText(code);
+
+    Button.innerText = "Copied!";
+    const fourSeconds = 1500;
+
+    setTimeout(() => {
+      Button.innerText = "Copy";
+    }, fourSeconds);
+  });
+});
